f4eb4cb73c3f95c0d94ed32aa7edec66d15b9044
denghaihui <denghaihui@jiemo.me>
2017-01-18 11:28:10 +0800

    添加rsa加密解密和密钥生成

--- flask/handshake_agreement/utils.py
+++ flask/handshake_agreement/utils.py
@@ -1,8 +1,7 @@
 from Crypto import Random
 from Crypto.Hash import SHA
-from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
-from Crypto.Signature import PKCS1_v1_5 as Signature_pkcs1_v1_5
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
 
 
 def key_pair_init(rsa_length=1024):
@@ -18,8 +17,36 @@ def key_pair_init(rsa_length=1024):
         print('create public key ......')
         f.write(publick_pem)
 
-def encrypt(msg):
-    pass
 
-def decrypt(msg):
-    pass
+# https://www.dlitz.net/software/pycrypto/api/2.6/
+def rsa_encrypt(msg, pub_key_path):
+    msg = bytes(msg, 'utf-8')
+    h = SHA.new(msg)
+    with open(pub_key_path, 'rb') as f:
+        key = RSA.importKey(f.read())
+    cipher = PKCS1_v1_5.new(key)
+    return cipher.encrypt(msg + h.digest())
+
+
+def rsa_decrypt(msg, pri_key_path):
+    with open(pri_key_path, 'rb') as f:
+        key = RSA.importKey(f.read())
+    dsize = SHA.digest_size
+    sentinel = Random.new().read(15 + dsize)
+    cipher = PKCS1_v1_5.new(key)
+    msg = cipher.decrypt(msg, sentinel)
+    digest = SHA.new(msg[:-dsize]).digest()
+    if digest == msg[-dsize:]:
+        return msg[:-dsize]
+    return msg
+
+
+def main():
+    key_pair_init(rsa_length=1024)
+    msg = rsa_decrypt(rsa_encrypt('test+1+测试', 'public_key.pem'), 'private_key.pem')
+    print(msg)
+    print(msg.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    main()
